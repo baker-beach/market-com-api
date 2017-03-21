a2110f286e59db5037b4410956d0b0d10726651e
Mark Hoja <mark.hoja@web.de>
2017-03-21 19:47:04 +0100

    add keys for transfer object

---
+++ src/main/java/com/bakerbeach/market/com/api/DataMapKeys.java
@@ -0,0 +1,11 @@
+package com.bakerbeach.market.com.api;
+
+public class DataMapKeys {
+	
+	public static final String CUSTOMER = "customer";
+	public static final String SHOP_CODE = "shop_code";
+	public static final String PASSWORD = "password";
+	public static final String ORDER = "order";
+	
+
+}
